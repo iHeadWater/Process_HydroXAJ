6e444d1050995fc12d023470b07db12c231c2c97
“XinQ2020” <1969848456@qq.com>
2022-05-19 14:35:48 +0800

    upgrade to hydromodel_test 0.0.1

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "HydroXAJ_test",
+    version = "0.0.1",
+    keywords = ["hydrologic model", "XinAnJiang"],
+    description = "A conceptual hydrological model.",
+    long_description = "This is a python implementation for XinAnJiang (XAJ) model, which is one of the most famous conceptual hydrological model, especially in Southern China.",
+
+    url = "https://github.com/OuyangWenyu/hydro-model-xaj",
+    author = "OuyangWenyu",
+    author_email = "wenyuouyang@outlook.com",
+    license= "MIT license ",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["numpy",
+                        "pandas",
+                        "deap",
+                        "spotpy",
+                        "seaborn",
+                        "tqdm",
+                        "sphinx",
+                        "pytest",
+                        "black"],
+
+)
